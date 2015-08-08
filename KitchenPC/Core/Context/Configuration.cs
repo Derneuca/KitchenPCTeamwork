521dc5c8161425166c8cb8be21281fd694d1ca22
unknown <todor.tachev@gmail.com>
2015-08-08 17:44:26 +0300

    Refactor Core - Recipe - SerchResults

--- KitchenPC/Core/Context/Configuration.cs
+++ KitchenPC/Core/Context/Configuration.cs
@@ -1,39 +1,41 @@
-﻿using System;
-
-namespace KitchenPC.Context
+﻿namespace KitchenPC.Context
 {
-   public class Configuration<T> : IConfiguration<T> where T : IKPCContext
-   {
-      readonly ConfigurationBuilder<T> builder;
+    using System;
 
-      public T Context { get; set; }
+    public class Configuration<T> : IConfiguration<T> where T : IKPCContext
+    {
+        private readonly ConfigurationBuilder<T> builder;
 
-      public static ConfigurationBuilder<T> Build
-      {
-         get
-         {
-            return new Configuration<T>().builder;
-         }
-      }
+        private Configuration()
+        {
+            this.builder = new ConfigurationBuilder<T>(this);
+        }
 
-      public static IConfiguration<T> Xml
-      {
-         get
-         {
-            // TODO: Read local XML configuration and return ConfigurationBuilder
-            throw new NotImplementedException();
-         }
-      }
+        public T Context { get; set; }
 
-      Configuration()
-      {
-         builder = new ConfigurationBuilder<T>(this);
-      }
+        public static ConfigurationBuilder<T> Build
+        {
+            get
+            {
+                return new Configuration<T>().builder;
+            }
+        }
 
-      public T InitializeContext()
-      {
-         Context.Initialize();
-         return Context;
-      }
-   }
+        public static IConfiguration<T> Xml
+        {
+            get
+            {
+                // TODO: Read local XML configuration and return ConfigurationBuilder
+                throw new NotImplementedException();
+            }
+        }
+
+        
+
+        public T InitializeContext()
+        {
+            Context.Initialize();
+            return Context;
+        }
+    }
 }
