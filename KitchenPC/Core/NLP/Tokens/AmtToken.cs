b80e7a6d12b0b13565180140bd2f2b5da6ddc2ca
Aleksandar Garov <sasho.garov@gmail.com>
2015-08-11 22:50:59 +0300

    Core-NLP-Token-AmtToken.cs - the usual refactoring

--- KitchenPC/Core/NLP/Tokens/AmtToken.cs
+++ KitchenPC/Core/NLP/Tokens/AmtToken.cs
@@ -1,92 +1,115 @@
-using System;
-using System.IO;
-using System.Text.RegularExpressions;
-
 namespace KitchenPC.NLP.Tokens
 {
-   public class AmtToken : IGrammar
-   {
-      /// <summary>
-      /// Reads stream to match it against an amount, number or indefinite article.  Sets any parsed value to usage.Amount
-      /// </summary>
-      /// <param name="stream"></param>
-      /// <param name="matchdata"></param>
-      /// <returns></returns>
-      public bool Read(Stream stream, MatchData matchdata)
-      {
-         //We probably have to detect a potential numeric match up front and then parsewith Validate, otherwise scan till first space and check if it's a number (do we support numbers with spaces?)
-         var matchPos = stream.Position;
-         var origPos = matchPos;
-         int curByte;
+    using System.IO;
+    using System.Text.RegularExpressions;
 
-         matchdata.Amount = null;
-         var curBuffer = String.Empty;
-         var invalidNumeric = new Regex(@"[^0-9 /.-]");
+    /// <summary>
+    /// The amt token.
+    /// </summary>
+    public class AmtToken : IGrammar
+    {
+        /// <summary>
+        /// Reads stream to match it against an amount, number or indefinite article.  Sets any parsed value to usage.Amount
+        /// </summary>
+        /// <param name="stream">
+        /// </param>
+        /// <param name="matchdata">
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool Read(Stream stream, MatchData matchdata)
+        {
+            // We probably have to detect a potential numeric match up front and then parsewith Validate,
+            // otherwise scan till first space and check if it's a number (do we support numbers with spaces?)
+            var matchPos = stream.Position;
+            var origPos = matchPos;
+            int curByte;
 
-         while ((curByte = stream.ReadByte()) >= 0)
-         {
-            float parsedAmtHigh;
-            float? parsedAmtLow;
+            matchdata.Amount = null;
+            var curBuffer = string.Empty;
+            var invalidNumeric = new Regex(@"[^0-9 /.-]");
 
-            curBuffer += (char) curByte;
-            if (Validate(curBuffer, out parsedAmtLow, out parsedAmtHigh)) //Try to parse buffer as a valid number or fraction
+            while ((curByte = stream.ReadByte()) >= 0)
             {
-               matchPos = stream.Position;
-               matchdata.Amount = new Amount();
-               matchdata.Amount.SizeLow = parsedAmtLow;
-               matchdata.Amount.SizeHigh = parsedAmtHigh;
+                float parsedAmtHigh;
+                float? parsedAmtLow;
+
+                curBuffer += (char)curByte;
+                if (Validate(curBuffer, out parsedAmtLow, out parsedAmtHigh))
+                {
+                    matchPos = stream.Position;
+                    matchdata.Amount = new Amount
+                    {
+                        SizeLow = parsedAmtLow,
+                        SizeHigh = parsedAmtHigh
+                    };
+                }
+                else
+                {
+                    if (invalidNumeric.IsMatch(curBuffer))
+                    {
+                        stream.Seek(origPos, SeekOrigin.Begin);
+                        var numericParser = new NumericToken();
+                        if (numericParser.Read(stream, matchdata))
+                        {
+                            matchPos = stream.Position;
+                        }
+
+                        break;
+                    }
+                }
             }
-            else //Stop processing as soon as we hit something that can no longer be an amount
+
+            stream.Seek(matchPos, SeekOrigin.Begin);
+            return matchdata.Amount != null;
+        }
+
+        /// <summary>
+        /// The validate.
+        /// </summary>
+        /// <param name="buffer">
+        /// The buffer.
+        /// </param>
+        /// <param name="amountLow">
+        /// The amount low.
+        /// </param>
+        /// <param name="amountHigh">
+        /// The amount high.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        private static bool Validate(string buffer, out float? amountLow, out float amountHigh)
+        {
+            decimal resultHigh;
+            amountLow = null;
+
+            var parts = buffer.Split('-'); // Look for potential range
+            if (parts.Length == 1)
             {
-               if (invalidNumeric.IsMatch(curBuffer)) //Buffer has something in it other than 0-9 /.
-               {
-                  //Hold on, let's try parsing this buffer using the NumericToken parser
-                  stream.Seek(origPos, SeekOrigin.Begin);
-                  var numericParser = new NumericToken();
-                  if (numericParser.Read(stream, matchdata))
-                  {
-                     matchPos = stream.Position;
-                  }
-
-                  break;
-               }
+                if (Fractions.TryParseFraction(buffer, out resultHigh))
+                {
+                    amountHigh = (float)resultHigh;
+                    return true;
+                }
             }
-         }
 
-         stream.Seek(matchPos, SeekOrigin.Begin);
-         return (matchdata.Amount != null); //We found a valid amount here
-      }
-
-      static bool Validate(string buffer, out float? amountLow, out float amountHigh)
-      {
-         decimal resultHigh;
-         amountLow = null;
-
-         var parts = buffer.Split('-'); //Look for potential range
-         if (parts.Length == 1)
-         {
-            if (Fractions.TryParseFraction(buffer, out resultHigh))
+            if (parts.Length == 2)
             {
-               amountHigh = (float) resultHigh;
-               return true;
+                decimal resultLow;
+
+                if (Fractions.TryParseFraction(parts[0], out resultLow) &&
+                    Fractions.TryParseFraction(parts[1], out resultHigh))
+                {
+                    amountLow = (float?)resultLow;
+                    amountHigh = (float)resultHigh;
+                    return true;
+                }
             }
-         }
 
-         if (parts.Length == 2)
-         {
-            decimal resultLow;
-
-            if (Fractions.TryParseFraction(parts[0], out resultLow) &&
-                Fractions.TryParseFraction(parts[1], out resultHigh))
-            {
-               amountLow = (float?) resultLow;
-               amountHigh = (float) resultHigh;
-               return true;
-            }
-         }
-
-         amountHigh = 0;
-         return false;
-      }
-   }
+            amountHigh = 0;
+            return false;
+        }
+    }
 }
