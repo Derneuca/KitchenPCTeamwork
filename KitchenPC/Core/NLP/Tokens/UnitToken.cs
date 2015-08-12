fa0bddfe8191addcf1b3273f920600b4c60c9c98
Aleksandar Garov <sasho.garov@gmail.com>
2015-08-12 17:35:06 +0300

    Core-Nlp-Tokens-UnitToken.cs the usual refactoring

--- KitchenPC/Core/NLP/Tokens/UnitToken.cs
+++ KitchenPC/Core/NLP/Tokens/UnitToken.cs
@@ -1,51 +1,50 @@
-using System;
-using System.IO;
-
 namespace KitchenPC.NLP.Tokens
 {
-   public class UnitToken : IGrammar
-   {
-      static UnitSynonyms data;
+    using System.IO;
 
-      /// <summary>
-      /// Reads stream to match it against a dictionary of all known units for an ingredient
-      /// </summary>
-      /// <param name="stream"></param>
-      /// <param name="matchdata"></param>
-      /// <returns></returns>
-      public bool Read(Stream stream, MatchData matchdata)
-      {
-         if (data == null)
-         {
-            data = new UnitSynonyms();
-         }
+    public class UnitToken : IGrammar
+    {
+        private static UnitSynonyms data;
 
-         UnitNode node;
-         var fMatch = false;
-         var buffer = String.Empty;
-         var matchPos = stream.Position;
-         int curByte;
-
-         while ((curByte = stream.ReadByte()) >= 0)
-         {
-            buffer += (char) curByte;
-            var match = data.Parse(buffer, out node);
-            if (match == MatchPrecision.None)
+        /// <summary>
+        /// Reads stream to match it against a dictionary of all known units for an ingredient
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="matchdata"></param>
+        /// <returns></returns>
+        public bool Read(Stream stream, MatchData matchdata)
+        {
+            if (data == null)
             {
-               stream.Seek(matchPos, SeekOrigin.Begin);
-               break; //No reason to continue reading stream, let's see what we have..
+                data = new UnitSynonyms();
             }
 
-            if (match == MatchPrecision.Exact)
-            {
-               matchPos = stream.Position;
-               fMatch = true;
-               matchdata.Amount.Unit = node.Unit;
-               matchdata.Unit = node;
-            }
-         }
+            var matchFound = false;
+            var buffer = string.Empty;
+            var matchPos = stream.Position;
+            int curByte;
 
-         return fMatch;
-      }
-   }
+            while ((curByte = stream.ReadByte()) >= 0)
+            {
+                buffer += (char)curByte;
+                UnitNode node;
+                var match = data.Parse(buffer, out node);
+                if (match == MatchPrecision.None)
+                {
+                    stream.Seek(matchPos, SeekOrigin.Begin);
+                    break;
+                }
+
+                if (match == MatchPrecision.Exact)
+                {
+                    matchPos = stream.Position;
+                    matchFound = true;
+                    matchdata.Amount.Unit = node.Unit;
+                    matchdata.Unit = node;
+                }
+            }
+
+            return matchFound;
+        }
+    }
 }
