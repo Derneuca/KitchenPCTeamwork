761cf0294f97ce152dfc869f52e2040bfc8c5f7f
unknown <todor.tachev@gmail.com>
2015-08-15 00:22:53 +0300

    Refactored Core - Modeler - UserProfile

--- KitchenPC/Core/Modeler/UserProfile.cs
+++ KitchenPC/Core/Modeler/UserProfile.cs
@@ -1,42 +1,49 @@
-using System;
-using KitchenPC.Recipes;
-
 namespace KitchenPC.Modeler
 {
-   public class UserProfile : IUserProfile
-   {
-      static IUserProfile anonymous;
+    using System;
+    using KitchenPC.Recipes;
+    
+    public class UserProfile : IUserProfile
+    {
+        private static IUserProfile anonymous;
 
-      /// <summary>
-      /// Represents a modeling profile that has no user context, such as a saved pantry, favorite ingredients, blacklists, etc.
-      /// </summary>
-      public static IUserProfile Anonymous
-      {
-         get
-         {
-            if (anonymous == null)
+        /// <summary>
+        /// Gets a modeling profile that has no user context, such as a saved pantry, favorite ingredients, blacklists, etc.
+        /// </summary>
+        public static IUserProfile Anonymous
+        {
+            get
             {
-               anonymous = new UserProfile
-               {
-                  UserId = Guid.Empty,
-                  Ratings = new RecipeRating[0],
-                  FavoriteIngredients = new Guid[0],
-                  FavoriteTags = RecipeTags.None,
-                  BlacklistedIngredients = new Guid[0]
-               };
+                if (anonymous == null)
+                {
+                    anonymous = new UserProfile
+                    {
+                        UserId = Guid.Empty,
+                        Ratings = new RecipeRating[0],
+                        FavoriteIngredients = new Guid[0],
+                        FavoriteTags = RecipeTags.None,
+                        BlacklistedIngredients = new Guid[0]
+                    };
+                }
+
+                return anonymous;
             }
+        }
 
-            return anonymous;
-         }
-      }
+        public Guid UserId { get; set; }
 
-      public Guid UserId { get; set; }
-      public RecipeRating[] Ratings { get; set; }
-      public PantryItem[] Pantry { get; set; }
-      public Guid[] FavoriteIngredients { get; set; }
-      public RecipeTags FavoriteTags { get; set; }
-      public Guid[] BlacklistedIngredients { get; set; }
-      public Guid? AvoidRecipe { get; set; }
-      public RecipeTags AllowedTags { get; set; }
-   }
+        public Guid? AvoidRecipe { get; set; }
+
+        public Guid[] BlacklistedIngredients { get; set; }
+
+        public Guid[] FavoriteIngredients { get; set; }
+
+        public PantryItem[] Pantry { get; set; }
+
+        public RecipeRating[] Ratings { get; set; }
+
+        public RecipeTags FavoriteTags { get; set; }
+
+        public RecipeTags AllowedTags { get; set; }
+    }
 }
