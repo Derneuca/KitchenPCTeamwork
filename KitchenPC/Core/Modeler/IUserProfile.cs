03ca74a06f28c7eeb60ebea5eaeab713c9426b2b
unknown <todor.tachev@gmail.com>
2015-08-15 00:46:07 +0300

    Refactored Core - Modeler - IUserProfile

--- KitchenPC/Core/Modeler/IUserProfile.cs
+++ KitchenPC/Core/Modeler/IUserProfile.cs
@@ -1,21 +1,35 @@
-﻿using System;
-using KitchenPC.Recipes;
-
-namespace KitchenPC.Modeler
+﻿namespace KitchenPC.Modeler
 {
-   /// <summary>
-   /// Interface used to transfer user specific runtime properties into the modeler.  KPC will be implementation that builds
-   /// this information from the database, and unit tests will create static instances for testing.
-   /// </summary>
-   public interface IUserProfile
-   {
-      Guid UserId { get; } //DB User ID
-      RecipeRating[] Ratings { get; } //Every recipe ID user has rated with the rating
-      PantryItem[] Pantry { get; } //Set of ingredients user has available with amounts
-      Guid[] FavoriteIngredients { get; } //Ingredients to favor
-      RecipeTags FavoriteTags { get; } //Tags to favor
-      Guid[] BlacklistedIngredients { get; } //Ingredients to always avoid
-      Guid? AvoidRecipe { get; }
-      RecipeTags AllowedTags { get; } //Only allow recipes that contain at least one of these tags (null to allow all)
-   }
+    using System;
+    using KitchenPC.Recipes;
+
+    /// <summary>
+    /// Interface used to transfer user specific runtime properties into the modeler.  KPC will be implementation that builds
+    /// this information from the database, and unit tests will create static instances for testing.
+    /// </summary>
+    public interface IUserProfile
+    {
+        Guid? AvoidRecipe { get; }
+
+        // DB User ID
+        Guid UserId { get; }
+
+        // Ingredients to always avoid
+        Guid[] BlacklistedIngredients { get; }
+
+        // Ingredients to favor
+        Guid[] FavoriteIngredients { get; }
+
+        // Set of ingredients user has available with amounts
+        PantryItem[] Pantry { get; }
+
+        // Every recipe ID user has rated with the rating
+        RecipeRating[] Ratings { get; }
+
+        // Tags to favor
+        RecipeTags FavoriteTags { get; }
+
+        // Only allow recipes that contain at least one of these tags (null to allow all)
+        RecipeTags AllowedTags { get; } 
+    }
 }
