dd819233f43cd154b8bce63191ffd29c3a860263
unknown <todor.tachev@gmail.com>
2015-08-19 02:33:24 +0300

    Refactored Core - Ingredients - IngredientForm

--- KitchenPC/Core/Ingredients/IngredientForm.cs
+++ KitchenPC/Core/Ingredients/IngredientForm.cs
@@ -1,43 +1,49 @@
-﻿using System;
-
-namespace KitchenPC.Ingredients
+﻿namespace KitchenPC.Ingredients
 {
-   public class IngredientForm
-   {
-      public Guid FormId;
-      public Guid IngredientId;
-      public Units FormUnitType;
-      public string FormDisplayName;
-      public string FormUnitName;
-      public int ConversionMultiplier;
-      public Amount FormAmount;
+    using System;
 
-      public static IngredientForm FromId(Guid id)
-      {
-         return new IngredientForm
-         {
-            FormId = id
-         };
-      }
+    public class IngredientForm
+    {
+        public IngredientForm()
+        {
+        }
 
-      public IngredientForm()
-      {
-      }
+        public IngredientForm(Guid formId, Guid ingredientId, Units unitType, string displayName, string unitName, int conversionMultiplier, Amount amount)
+        {
+            this.FormId = formId;
+            this.IngredientId = ingredientId;
+            this.FormUnitType = unitType;
+            this.FormDisplayName = displayName;
+            this.FormUnitName = unitName;
+            this.ConversionMultiplier = conversionMultiplier;
+            this.FormAmount = amount;
+        }
 
-      public IngredientForm(Guid formid, Guid ingredientid, Units unittype, string displayname, string unitname, int convmultiplier, Amount amount)
-      {
-         FormId = formid;
-         IngredientId = ingredientid;
-         FormUnitType = unittype;
-         FormDisplayName = displayname;
-         FormUnitName = unitname;
-         ConversionMultiplier = convmultiplier;
-         FormAmount = amount;
-      }
+        public Guid FormId { get; set; }
 
-      public override string ToString()
-      {
-         return FormId.ToString();
-      }
-   }
+        public Guid IngredientId { get; set; }
+
+        public Units FormUnitType { get; set; }
+
+        public string FormDisplayName { get; set; }
+
+        public string FormUnitName { get; set; }
+
+        public int ConversionMultiplier { get; set; }
+
+        public Amount FormAmount { get; set; }
+
+        public static IngredientForm FromId(Guid id)
+        {
+            return new IngredientForm
+            {
+                FormId = id
+            };
+        }
+
+        public override string ToString()
+        {
+            return this.FormId.ToString();
+        }
+    }
 }
