1944ba0ae5f2f1062b5663eaa5aba3c27a64911c
unknown <todor.tachev@gmail.com>
2015-08-19 02:29:11 +0300

    Refactored Core - Ingredients - IngredientAggregation

--- KitchenPC/Core/Ingredients/IngredientAggregation.cs
+++ KitchenPC/Core/Ingredients/IngredientAggregation.cs
@@ -1,66 +1,75 @@
-﻿using System;
-using KitchenPC.ShoppingLists;
-
-namespace KitchenPC.Ingredients
+﻿namespace KitchenPC.Ingredients
 {
-   public class IngredientAggregation : IShoppingListSource
-   {
-      public Ingredient Ingredient;
-      public Amount Amount;
+    using System;
+    using KitchenPC.ShoppingLists;
 
-      public IngredientAggregation(Ingredient ingredient)
-      {
-         this.Ingredient = ingredient;
+    public class IngredientAggregation : IShoppingListSource
+    {
+        public IngredientAggregation(Ingredient ingredient)
+        {
+            this.Ingredient = ingredient;
 
-         if (ingredient != null)
-         {
-            this.Amount = new Amount();
-            this.Amount.Unit = Unit.GetDefaultUnitType(ingredient.ConversionType);
-         }
-      }
+            if (ingredient != null)
+            {
+                this.Amount = new Amount();
+                this.Amount.Unit = Unit.GetDefaultUnitType(ingredient.ConversionType);
+            }
+        }
 
-      public IngredientAggregation(Ingredient ingredient, Amount baseAmount)
-      {
-         this.Ingredient = ingredient;
-         this.Amount = baseAmount;
-      }
+        public IngredientAggregation(Ingredient ingredient, Amount baseAmount)
+        {
+            this.Ingredient = ingredient;
+            this.Amount = baseAmount;
+        }
 
-      public override string ToString()
-      {
-         if (Ingredient != null && Amount != null)
-            return String.Format("{0}: {1}", Ingredient.Name, Amount);
+        public Ingredient Ingredient { get; set; }
 
-         if (Ingredient != null)
-            return Ingredient.Name;
+        public Amount Amount { get; set; }
 
-         return String.Empty;
-      }
+        public override string ToString()
+        {
+            if (this.Ingredient != null && this.Amount != null)
+            {
+                return string.Format("{0}: {1}", this.Ingredient.Name, this.Amount);
+            }
 
-      public virtual IngredientAggregation AddUsage(IngredientUsage ingredient)
-      {
-         if (ingredient.Ingredient.Id != this.Ingredient.Id)
-            throw new ArgumentException("Can only call IngredientAggregation::AddUsage() on original ingredient.");
+            if (this.Ingredient != null)
+            {
+                return this.Ingredient.Name;
+            }
 
-         //Calculate new total
-         if (this.Amount.Unit == ingredient.Amount.Unit || UnitConverter.CanConvert(this.Amount.Unit, ingredient.Amount.Unit)) //Just add
-         {
-            this.Amount += ingredient.Amount;
-         }
-         else //Find a conversion path between Ingredient and Form
-         {
-            var amount = FormConversion.GetNativeAmountForUsage(this.Ingredient, ingredient);
-            this.Amount += amount;
-         }
+            return string.Empty;
+        }
 
-         return this; // Allows AddUsage calls to be chained together
-      }
+        public virtual IngredientAggregation AddUsage(IngredientUsage ingredient)
+        {
+            if (ingredient.Ingredient.Id != this.Ingredient.Id)
+            {
+                throw new ArgumentException("Can only call IngredientAggregation::AddUsage() on original ingredient.");
+            }
 
-      public virtual ShoppingListItem GetItem()
-      {
-         return new ShoppingListItem(Ingredient)
-         {
-            Amount = Amount
-         };
-      }
-   }
+            // Calculate new total
+            if (this.Amount.Unit == ingredient.Amount.Unit || 
+                UnitConverter.CanConvert(this.Amount.Unit, ingredient.Amount.Unit))
+            {
+                this.Amount += ingredient.Amount;
+            }
+            else 
+            {
+                // Find a conversion path between Ingredient and Form
+                var amount = FormConversion.GetNativeAmountForUsage(this.Ingredient, ingredient);
+                this.Amount += amount;
+            }
+
+            return this; // Allows AddUsage calls to be chained together
+        }
+
+        public virtual ShoppingListItem GetItem()
+        {
+            return new ShoppingListItem(this.Ingredient)
+            {
+                Amount = this.Amount
+            };
+        }
+    }
 }
