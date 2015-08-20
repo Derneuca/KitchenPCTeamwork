c302ed5890230515b1eea0fd1695e925b6c557c2
unknown <todor.tachev@gmail.com>
2015-08-20 09:10:58 +0300

    Refactored Core - AuthIdentity

--- KitchenPC/Core/AuthIdentity.cs
+++ KitchenPC/Core/AuthIdentity.cs
@@ -1,84 +1,89 @@
-﻿using System;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Security.Principal;
-using System.Text;
-
-namespace KitchenPC
+﻿namespace KitchenPC
 {
-   /// <summary>Represents a user identity within a KitchenPC context.  This identity can be stored locally or within the KitchenPC network.</summary>
-   public class AuthIdentity : IIdentity
-   {
-      public Guid UserId { get; private set; }
-      public string Name { get; private set; }
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Security.Principal;
+    using System.Text;
 
-      public AuthIdentity()
-      {
-      }
+    /// <summary>Represents a user identity within a KitchenPC context.  This identity can be stored locally or within the KitchenPC network.</summary>
+    public class AuthIdentity : IIdentity
+    {
+        private const int MinimumAuthIdentityBytes = 17;
 
-      public AuthIdentity(Guid id, string name)
-      {
-         UserId = id;
-         Name = name;
-      }
+        public AuthIdentity()
+        {
+        }
 
-      public string AuthenticationType
-      {
-         get
-         {
-            return "KPCAuth";
-         }
-      }
+        public AuthIdentity(Guid id, string name)
+        {
+            this.UserId = id;
+            this.Name = name;
+        }
 
-      public bool IsAuthenticated
-      {
-         get
-         {
-            return UserId != Guid.Empty;
-         }
-      }
+        public Guid UserId { get; private set; }
 
-      public static string CreateHash(string value)
-      {
-         var hasher = MD5.Create();
-         var bytes = hasher.ComputeHash(Encoding.Unicode.GetBytes(value));
-         return Convert.ToBase64String(bytes);
-      }
+        public string Name { get; private set; }
 
-      public override string ToString()
-      {
-         if (IsAuthenticated)
-         {
-            return String.Format("{0} ({1})", Name, UserId.ToString());
-         }
-         else
-         {
-            return "<Anonymous>";
-         }
-      }
+        public string AuthenticationType
+        {
+            get
+            {
+                return "KPCAuth";
+            }
+        }
 
-      public static Byte[] Serialize(AuthIdentity identity)
-      {
-         var g = identity.UserId.ToByteArray();
-         var u = Encoding.UTF8.GetBytes(identity.Name);
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return this.UserId != Guid.Empty;
+            }
+        }
 
-         return g.Concat(u).ToArray();
-      }
+        public static string CreateHash(string value)
+        {
+            var hasher = MD5.Create();
+            byte[] bytes = hasher.ComputeHash(Encoding.Unicode.GetBytes(value));
+            return Convert.ToBase64String(bytes);
+        }
 
-      public static AuthIdentity Deserialize(Byte[] bytes)
-      {
-         if (bytes.Length < 17)
-            throw new ArgumentException("AuthIdentity must be at least 17 bytes.");
+        public static byte[] Serialize(AuthIdentity identity)
+        {
+            byte[] g = identity.UserId.ToByteArray();
+            byte[] u = Encoding.UTF8.GetBytes(identity.Name);
 
-         var g = new byte[16];
-         var u = new byte[bytes.Length - 16];
+            return g.Concat(u).ToArray();
+        }
 
-         Buffer.BlockCopy(bytes, 0, g, 0, 16);
-         Buffer.BlockCopy(bytes, 16, u, 0, u.Length);
+        public static AuthIdentity Deserialize(byte[] bytes)
+        {
+            if (bytes.Length < MinimumAuthIdentityBytes)
+            {
+                throw new ArgumentException(string.Format("AuthIdentity must be at least {0} bytes.", MinimumAuthIdentityBytes));
+            }
 
-         return new AuthIdentity(
-            new Guid(g),
-            Encoding.UTF8.GetString(u));
-      }
-   }
+            byte[] g = new byte[16];
+            byte[] u = new byte[bytes.Length - 16];
+
+            Buffer.BlockCopy(bytes, 0, g, 0, 16);
+            Buffer.BlockCopy(bytes, 16, u, 0, u.Length);
+
+            return new AuthIdentity(
+               new Guid(g),
+               Encoding.UTF8.GetString(u));
+        }
+
+        public override string ToString()
+        {
+            if (this.IsAuthenticated)
+            {
+                return string.Format("{0} ({1})", this.Name, this.UserId.ToString());
+            }
+            else
+            {
+                return "<Anonymous>";
+            }
+        }
+    }
 }
