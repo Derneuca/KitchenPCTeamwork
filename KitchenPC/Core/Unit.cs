bedd9517e67324268423eb61dc989702c788c54c
unknown <todor.tachev@gmail.com>
2015-08-20 09:22:55 +0300

    Refactored Core - Unit

--- KitchenPC/Core/Unit.cs
+++ KitchenPC/Core/Unit.cs
@@ -1,114 +1,125 @@
-using System;
-
 namespace KitchenPC
 {
-   public static class Unit
-   {
-      static readonly String[] Singular =
-      {
-         "",
-         "tsp",
-         "Tbl",
-         "fl oz",
-         "cup",
-         "pt",
-         "qt",
-         "gal",
-         "g",
-         "oz",
-         "lb"
-      };
+    using System;
 
-      static readonly String[] Plural =
-      {
-         "",
-         "tsp",
-         "Tbl",
-         "fl oz",
-         "cups",
-         "pts",
-         "qts",
-         "gal",
-         "g",
-         "oz",
-         "lbs"
-      };
+    public static class Unit
+    {
+        private static readonly string[] Singular =
+        {
+            string.Empty,
+            "tsp",
+            "Tbl",
+            "fl oz",
+            "cup",
+            "pt",
+            "qt",
+            "gal",
+            "g",
+            "oz",
+            "lb"
+        };
 
-      public static string GetSingular(Units unitType)
-      {
-         return Singular[(int) unitType];
-      }
+        private static readonly string[] Plural =
+        {
+            string.Empty,
+            "tsp",
+            "Tbl",
+            "fl oz",
+            "cups",
+            "pts",
+            "qts",
+            "gal",
+            "g",
+            "oz",
+            "lbs"
+        };
 
-      public static string GetPlural(Units unitType)
-      {
-         return Plural[(int) unitType];
-      }
+        public static string GetSingular(Units unitType)
+        {
+            return Singular[(int)unitType];
+        }
 
-      public static UnitType GetConvertionType(Units unitType)
-      {
-         switch (unitType)
-         {
-            case Units.Unit:
-               return UnitType.Unit;
-            case Units.Teaspoon:
-            case Units.Tablespoon:
-            case Units.FluidOunce:
-            case Units.Cup:
-            case Units.Pint:
-            case Units.Quart:
-            case Units.Gallon:
-               return UnitType.Volume;
-            case Units.Gram:
-            case Units.Ounce:
-            case Units.Pound:
-               return UnitType.Weight;
-         }
+        public static string GetPlural(Units unitType)
+        {
+            return Plural[(int)unitType];
+        }
 
-         throw new ArgumentException("Invalid unitType specified.");
-      }
+        public static UnitType GetConvertionType(Units unitType)
+        {
+            switch (unitType)
+            {
+                case Units.Unit:
+                    return UnitType.Unit;
+                case Units.Teaspoon:
+                case Units.Tablespoon:
+                case Units.FluidOunce:
+                case Units.Cup:
+                case Units.Pint:
+                case Units.Quart:
+                case Units.Gallon:
+                    return UnitType.Volume;
+                case Units.Gram:
+                case Units.Ounce:
+                case Units.Pound:
+                    return UnitType.Weight;
+            }
 
-      public static Units GetDefaultUnitType(UnitType convType)
-      {
-         if (convType == UnitType.Unit)
-            return Units.Unit;
-         if (convType == UnitType.Volume)
-            return Units.Cup;
-         if (convType == UnitType.Weight)
-            return Units.Ounce;
+            throw new ArgumentException("Invalid unitType specified.");
+        }
 
-         throw new ArgumentException("Invalid convType passed in to GetDefaultUnitType.");
-      }
+        public static Units GetDefaultUnitType(UnitType convertionType)
+        {
+            switch (convertionType)
+            {
+                case UnitType.Unit:
+                    return Units.Unit;
+                case UnitType.Volume:
+                    return Units.Cup;
+                case UnitType.Weight:
+                    return Units.Ounce;
+                default:
+                    throw new ArgumentException("Invalid convertionType passed in to GetDefaultUnitType.");
+            }
+        }
 
-      public static T? ParseNullable<T>(object value) where T : struct
-      {
-         if (!typeof (T).IsEnum)
-            throw new ArgumentException("T must be an Enum type.");
+        public static T? ParseNullable<T>(object value) where T : struct
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an Enum type.");
+            }
 
-         if (value == null || value == DBNull.Value)
-            return null;
+            if (value == null || value == DBNull.Value)
+            {
+                return null;
+            }
 
-         if (value is String)
-            return Enum.Parse(typeof (T), value.ToString()) as T?;
+            if (value is string)
+            {
+                return Enum.Parse(typeof(T), value.ToString()) as T?;
+            }
 
-         return Enum.ToObject(typeof (T), value) as T?;
-      }
+            return Enum.ToObject(typeof(T), value) as T?;
+        }
 
-      public static T Parse<T>(object value) where T : struct
-      {
-         if (!typeof (T).IsEnum)
-            throw new ArgumentException("T must be an Enum type.");
+        public static T Parse<T>(object value) where T : struct
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an Enum type.");
+            }
 
-         if (value == null || value == DBNull.Value)
-         {
-            throw new ArgumentException("Cannot parse enum, tagValue is null.");
-         }
+            if (value == null || value == DBNull.Value)
+            {
+                throw new ArgumentException("Cannot parse enum, tagValue is null.");
+            }
 
-         if (value is String)
-         {
-            return (T) Enum.Parse(typeof (T), value.ToString());
-         }
+            if (value is string)
+            {
+                return (T)Enum.Parse(typeof(T), value.ToString());
+            }
 
-         return (T) Enum.ToObject(typeof (T), value);
-      }
-   }
+            return (T)Enum.ToObject(typeof(T), value);
+        }
+    }
 }
